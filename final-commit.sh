0450163cc5928ace581e18d98e68044c4294445c
akak4456 <akak4456@naver.com>
2021-01-28 16:02:01 +0900

    MY COMMIT MESSAGE

--- final-commit.sh
+++ final-commit.sh
@@ -1,9 +1,8 @@
 echo $#
 git add .
-if [$# -eq 2]
-# 만약에 -m을 입력했다면 즉 커밋 메시지를 입력했다면
+if [ $# -eq 2 ];
 then
-	git commit -m $2
+	git commit -m "$2" # 만약에 -m을 입력했다면 즉 커밋 메시지를 입력했다면
 else
 	git commit -m "no message"
 fi
