631145f5f8d614d7a94ba7af768129f68e445450
akak4456 <akak4456@naver.com>
2021-01-28 15:50:00 +0900

    no message

--- final-commit.sh
+++ final-commit.sh
@@ -1,4 +1,9 @@
-echo $#
 git add .
-git commit -m "final commit"
+if [$# -eq 2]
+# 만약에 -m을 입력했다면 즉 커밋 메시지를 입력했다면
+then
+	git commit -m $2
+else
+	git commit -m "no message"
+fi
 git push origin master
